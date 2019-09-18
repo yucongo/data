4f2b019ada3b0c70c127633ddb60bd5eaa5d8b0d
ffreemt <yucongo+fmt@gmail.com>
2019-09-18 20:58:51 +0800

    keras-baseline-chollet-ch3

--- keras-baseline-chollet-ch3.py
+++ keras-baseline-chollet-ch3.py
@@ -22,4 +22,33 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 
 
+# ch3.4.1
+from keras.datasets import imdb
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(
+num_words=10000)
 
+word_index = imdb.get_word_index()
+reverse_word_index = dict(
+[(value, key) for (key, value) in word_index.items()])
+decoded_review = ' '.join(
+[reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+
+import numpy as np
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+from keras import models
+from keras import layers
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+loss='binary_crossentropy',
+metrics=['accuracy'])
