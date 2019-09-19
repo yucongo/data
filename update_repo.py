8b3c6eede5b28e5ebe65c24f988405efe618bc5a
ffreemt <yucongo+fmt@gmail.com>
2019-09-19 10:38:03 +0800

    minor update

--- update_repo.py
+++ update_repo.py
@@ -2,19 +2,38 @@ def update_repo(url='https://github.com/yucongo/data.git'):
     ''' git clone/pull git repo
     >>> update_repo(repo_https_git_address)
     '''
-    import os, sys
+    import os, sys, shlex
+    import subprocess as sp
     from os import chdir
     from pathlib import Path
 
     dirname = Path(url).stem
     chdir('/content')
     if not Path('data').exists():
-        print('Cloning %s...' % url)
-        os.system('git clone %s' % url)
+        print('git clone %s...' % url)
+        # os.system('git clone %s' % url)
+        proc = sp.Popen(
+            shlex.split('git clone %s' % url),
+            stdout=sp.PIPE,
+            stderr=sp.PIPE,
+        )
+        out, err = proc.communicate()
+        if err:
+            print('>> %s, %s' % (err.decode(), out.decode()))
+        else:
+            print('git clone...%s' % out.decode())
     else:
         chdir(dirname)
-        print('Pulling %s...' % url)
-        os.system('git pull')
+        print('git pull %s...' % url)
+        # os.system('git pull')
+        proc = sp.Popen(
+            shlex.split('git pull %s' % url),
+            stdout=sp.PIPE,
+            stderr=sp.PIPE,
+        )
+        out, err = proc.communicate()
+        if err:
+            print('>> %s' % err.decode())
+        print('git pull ...%s' % out.decode())
     chdir('/content/%s' % dirname)
-    print('git clone/pull %s completed \n-- Now in /content/%s\n'
-     % (url, dirname))
+    print(' Now in /content/%s\n' % dirname)
